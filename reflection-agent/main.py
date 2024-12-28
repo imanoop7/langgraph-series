47fd305c3b0bf20d8ccab17285a1b0d265a4f65a
Anoop Maurya <64773373+imanoop7@users.noreply.github.com>
2024-12-28 17:29:22 +0530

    main

---
+++ reflection-agent/main.py
@@ -0,0 +1,44 @@
+from typing import List, Sequence
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, MessageGraph
+from chain import generate_chain, reflect_chain
+
+
+
+def generate_node(state:Sequence[BaseMessage]):
+    return generate_chain.invoke({"messages" : state})
+
+
+def refect_node(message:Sequence[BaseMessage]):
+    res= reflect_chain.invoke({"messages" : message})
+    return [HumanMessage(content=res.content)]  
+
+
+builder = MessageGraph()
+
+builder.add_node("generate", generate_node)
+builder.add_node("reflect", refect_node)    
+
+builder.set_entry_point("generate")
+
+def should_continue(state:List[BaseMessage]):
+    if len(state) < 5:
+        return END
+    return "reflect"
+
+
+builder.add_conditional_edges("generate", should_continue)
+builder.add_edge("reflect", "generate")
+
+graph = builder.compile()
+
+
+if __name__ == "__main__":
+    inputs =HumanMessage(content=""" Make this tweet better:
+                         @Langchain 
+                         - newly tool calling feature is seriously underrated.
+                         - after a long wait it's finally here. making the implementation of agents across different models with function calling.
+                         - have written a medium article on it. check it out.
+    """)
+    response = graph.invoke(inputs)
+    print(response)
