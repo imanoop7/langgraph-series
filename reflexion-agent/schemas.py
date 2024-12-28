0f2dba84ced41697e24d46730328147223b61414
Anoop Maurya <64773373+imanoop7@users.noreply.github.com>
2024-12-28 22:14:09 +0530

    schema- reflexion

---
+++ reflexion-agent/schemas.py
@@ -0,0 +1,16 @@
+from typing import List
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+
+class Reflection(BaseModel):
+    missing : str = Field(description="Critique of what is missing")
+    superfloous : str = Field(description="Critique of what is superfloous")
+
+
+class AnswerQuestion(BaseModel):
+    answer : str = Field(description="250 word detailed answer to the question.")
+    reflection : Reflection = Field(description="Your Reflection on the intial answer")
+    search_quires: List[str] = Field(description="1-3 seach for researching improvements to address the critique of your current answer.")
+    
+     
